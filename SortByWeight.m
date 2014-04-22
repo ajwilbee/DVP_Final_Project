6bfa744312b5508c5a8e485b8e86750c6279947b
Aaron Wilbee <ajwilbee713@hotmail.com>
2014-04-22 19:59:08 -0400

    finished writing the gaussian, a bit worried about the sigma tuner, ready to have the algorithm itterat over the whole video

---
+++ SortByWeight.m
@@ -0,0 +1,19 @@
+function [ GMMByWeight ] = SortByWeight( GMM )
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+
+weights = zeros(size(GMM));
+
+for x = 1:length(GMM)
+   weights(x) = GMM{x}.priorWeight;
+end
+[sorted, index] = sort(weights,'descend');
+normalizer = sum(weights);
+GMMByWeight = cell(size(GMM));
+for x = 1:length(GMM)
+    GMM{index(x)}.priorWeight=GMM{index(x)}.priorWeight/normalizer;
+    GMMByWeight{x} = GMM{index(x)};
+end
+
+end
+
